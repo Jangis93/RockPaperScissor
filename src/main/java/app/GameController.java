bdc627f45bc0d28e582fd9868b38e45b98f10f78
unknown <michaela.jangefalk@gmail.com>
2019-06-02 17:49:38 +0200

    Cleanup

--- src/main/java/app/GameController.java
+++ src/main/java/app/GameController.java
@@ -7,8 +7,12 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/*
-    Rock-Paper-Scissor HTTTP API written by Michaela Jangefalk according to specification given.
+/**
+ * Rock-Paper-Scissor HTTTP API implementation written by Michaela Jangefalk according to given API:
+ * POST /api/games
+ * POST /api/games/{id}/join
+ * POST /api/games/{id}/move
+ * GET /api/games/{id}
  */
 
 @RestController
@@ -25,6 +29,10 @@ public class GameController {
         counter = 0;
     }
 
+    /**
+     * @param name the name of the player who wants to create a new game
+     * @return the game id of the created game
+     */
     @RequestMapping(method = RequestMethod.POST, value = "")
     @ResponseStatus(HttpStatus.CREATED)
     public int startNewGame(@RequestParam(value = "name") String name) {
@@ -36,6 +44,12 @@ public class GameController {
         return gameID;
     }
 
+    /**
+     * @param id the id of the game the user are interested in
+     * @return the entire game state
+     * @throws GameDoesNotExistException if game does not exist
+     * @throws GameNotFinishedException  if the game is not finsihed
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
     public Game checkGame(@PathVariable int id) throws GameDoesNotExistException, GameNotFinishedException {
@@ -51,11 +65,15 @@ public class GameController {
         }
     }
 
+    /**
+     * @param id   the id of the game that a player wants to join
+     * @param name the name of the new player
+     * @throws GameDoesNotExistException if the game does not exist
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/{id}/join")
     @ResponseStatus(HttpStatus.OK)
     public void joinGame(@PathVariable int id,
                          @RequestParam(value = "name") String name) throws GameDoesNotExistException {
-
         if (!games.containsKey(id)) {
             throw new GameDoesNotExistException(id);
         }
@@ -64,7 +82,13 @@ public class GameController {
         game.setPlayerTwo(player2);
     }
 
-
+    /**
+     * @param id   the id of the game that the player wants to do a move in
+     * @param name the name of the player that wants to do the move
+     * @param move the move which the player wants to do
+     * @throws GameDoesNotExistException if the game does not exist
+     * @throws GameNotActiveException    if the game is already finsihed
+     */
     @RequestMapping(method = RequestMethod.POST, value = "/{id}/move")
     @ResponseStatus(HttpStatus.OK)
     public void move(@PathVariable int id,
@@ -96,12 +120,11 @@ public class GameController {
                 game.getPlayerTwo().setMove(playerTwo);
             } else {
                 playerOne = Move.valueOf(move);
-                game.getPlayerOne().setMove(playerOne);
                 playerTwo = game.getPlayerTwo().getMove();
+                game.getPlayerOne().setMove(playerOne);
             }
 
             Result result = gameService.evaluateMoves(playerOne, playerTwo);
-            System.out.println(result);
             game.setResult(result);
             game.setState(State.FINISHED);
         }
